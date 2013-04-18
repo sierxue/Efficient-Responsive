573308efea3603c11a310ceecf8957530f68ab6f
WANG Tong <tong.wang@nus.edu.sg>
2013-04-18 15:52:12 +0800

    change in R scripts for better plots

--- output/Efficient-Responsive-Single-K.R
+++ output/Efficient-Responsive-Single-K.R
@@ -12,18 +12,18 @@
 
 
 #prepare data for Figure 1
+
     data.K1 <- data[data$K==1,]
-    if (nrow(data.K1[data.K1$TS01==0,]) >0 ) data.K1[data.K1$TS01==0,]$TS01 <- NA
     if (nrow(data.K1[data.K1$TS12==20,]) >0 ) data.K1[data.K1$TS12==20,]$TS12 <- NA
 
     data.K3 <- data[data$K==3,]
     if (nrow(data.K3[data.K3$NashE==20,]) >0 ) data.K3[data.K3$NashE==20,]$NashE <- NA
     if (nrow(data.K3[data.K3$NashR==20,]) >0 ) data.K3[data.K3$NashR==20,]$NashR <- NA
-    if (nrow(data.K3[data.K3$TS01==0,]) >0 ) data.K3[data.K3$TS01==0,]$TS01 <- NA
     if (nrow(data.K3[data.K3$TS12==20,]) >0 ) data.K3[data.K3$TS12==20,]$TS12 <- NA
 
 
 #plot figure for K=1
+
     pdf('Figure-K1.pdf', width = 12, height = 7)
     par(oma=c(0,0,2,0))
     par(mfrow=c(1,2))
@@ -33,15 +33,15 @@
     yrange = c(0, 20)
 
     plot(xrange, yrange, type="n", xlab="b", ylab=expression(paste("VOI (", sigma^2, ")")) , xaxt="n", yaxt="n")
-    lines(data.K1$b, data.K1$NashE, type="l", lwd=4, col="red")
-    lines(data.K1$b, data.K1$NashR, type="l", lwd=4, col="blue")
+    lines(data.K1$b, data.K1$NashE, lty=1, lwd=3, col="red")
+    lines(data.K1$b, data.K1$NashR, lty=1, lwd=3, col="blue")
     axis(side=1, at=seq(0,1,0.1), labels=seq(0,1,0.1))
     axis(side=2, at=seq(0,20,2), labels=seq(0,20,2))
     title(main="(a) Firms' Strategic Choices")
 
     plot(xrange, yrange, type="n", xlab="b", ylab=expression(paste("VOI (", sigma^2, ")")) , xaxt="n", yaxt="n")
-    lines(data.K1$b, data.K1$TS01, type="l", lwd=4, col="darkolivegreen")
-    lines(data.K1$b, data.K1$TS12, type="l", lwd=4, col="darkolivegreen")
+    lines(data.K1$b, data.K1$TS01, lty=1, lwd=3, col="darkolivegreen")
+    lines(data.K1$b, data.K1$TS12, lty=1, lwd=3, col="darkolivegreen")
     axis(side=1, at=seq(0,1,0.1), labels=seq(0,1,0.1))
     axis(side=2, at=seq(0,20,2), labels=seq(0,20,2))
     title(main="(b) Socially Efficient Strategies")
--- output/Efficient-Responsive-Single-delta.R
+++ output/Efficient-Responsive-Single-delta.R
@@ -12,18 +12,18 @@
 
 
 #prepare data for Figure 1
+
     data.delta2 <- data[data$delta==1.2,]
-    if (nrow(data.delta2[data.delta2$TS01==0,]) >0 ) data.delta2[data.delta2$TS01==0,]$TS01 <- NA
     if (nrow(data.delta2[data.delta2$TS12==20,]) >0 ) data.delta2[data.delta2$TS12==20,]$TS12 <- NA
 
     data.delta5 <- data[data$delta==1.5,]
     if (nrow(data.delta5[data.delta5 $NashE==20,]) >0 ) data.delta5[data.delta5 $NashE==20,]$NashE <- NA
     if (nrow(data.delta5[data.delta5 $NashR==20,]) >0 ) data.delta5[data.delta5 $NashR==20,]$NashR <- NA
-    if (nrow(data.delta5[data.delta5 $TS01==0,]) >0 ) data.delta5[data.delta5 $TS01==0,]$TS01 <- NA
     if (nrow(data.delta5[data.delta5 $TS12==20,]) >0 ) data.delta5[data.delta5 $TS12==20,]$TS12 <- NA
 
 
 #plot figure for delta=1.2
+
     pdf('Figure-delta2.pdf', width = 12, height = 7)
     par(oma=c(0,0,2,0))
     par(mfrow=c(1,2))
@@ -33,15 +33,15 @@
     yrange = c(0, 20)
 
     plot(xrange, yrange, type="n", xlab="b", ylab=expression(paste("VOI (", sigma^2, ")")) , xaxt="n", yaxt="n")
-    lines(data.delta2 $b, data.delta2 $NashE, type="l", lwd=4, col="red")
-    lines(data.delta2 $b, data.delta2 $NashR, type="l", lwd=4, col="blue")
+    lines(data.delta2 $b, data.delta2 $NashE, lty=1, lwd=3, col="red")
+    lines(data.delta2 $b, data.delta2 $NashR, lty=1, lwd=3, col="blue")
     axis(side=1, at=seq(0,1,0.1), labels=seq(0,1,0.1))
     axis(side=2, at=seq(0,20,2), labels=seq(0,20,2))
     title(main="(a) Firms' Strategic Choices")
 
     plot(xrange, yrange, type="n", xlab="b", ylab=expression(paste("VOI (", sigma^2, ")")) , xaxt="n", yaxt="n")
-    lines(data.delta2 $b, data.delta2 $TS01, type="l", lwd=4, col="darkolivegreen")
-    lines(data.delta2 $b, data.delta2 $TS12, type="l", lwd=4, col="darkolivegreen")
+    lines(data.delta2 $b, data.delta2 $TS01, lty=1, lwd=3, col="darkolivegreen")
+    lines(data.delta2 $b, data.delta2 $TS12, lty=1, lwd=3, col="darkolivegreen")
     axis(side=1, at=seq(0,1,0.1), labels=seq(0,1,0.1))
     axis(side=2, at=seq(0,20,2), labels=seq(0,20,2))
     title(main="(b) Socially Efficient Strategies")
@@ -61,15 +61,15 @@
     yrange = c(0, 20)
 
     plot(xrange, yrange, type="n", xlab="b", ylab=expression(paste("VOI (", sigma^2, ")")) , xaxt="n", yaxt="n")
-    lines(data.delta5 $b, data.delta5 $NashE, type="l", lwd=4, col="red")
-    lines(data.delta5 $b, data.delta5 $NashR, type="l", lwd=4, col="blue")
+    lines(data.delta5 $b, data.delta5 $NashE, lty=1, lwd=3, col="red")
+    lines(data.delta5 $b, data.delta5 $NashR, lty=1, lwd=3, col="blue")
     axis(side=1, at=seq(0,1,0.1), labels=seq(0,1,0.1))
     axis(side=2, at=seq(0,20,2), labels=seq(0,20,2))
     title(main="(a) Firms' Strategic Choices")
 
     plot(xrange, yrange, type="n", xlab="b", ylab=expression(paste("VOI (", sigma^2, ")")) , xaxt="n", yaxt="n")
-    lines(data.delta5 $b, data.delta5 $TS01, type="l", lwd=4, col="darkolivegreen")
-    lines(data.delta5 $b, data.delta5 $TS12, type="l", lwd=4, col="darkolivegreen")
+    lines(data.delta5 $b, data.delta5 $TS01, lty=1, lwd=3, col="darkolivegreen")
+    lines(data.delta5 $b, data.delta5 $TS12, lty=1, lwd=3, col="darkolivegreen")
     axis(side=1, at=seq(0,1,0.1), labels=seq(0,1,0.1))
     axis(side=2, at=seq(0,20,2), labels=seq(0,20,2))
     title(main="(b) Socially Efficient Strategies")
